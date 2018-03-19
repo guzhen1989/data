85d54f3d9a9c28c3dbbbb2049da28f46b7c48b3a
guzhen <1132053388@qq.com>
2018-03-19 21:15:47 +0800

    user

--- src/main/java/com/xg/config/WebSecurity.java
+++ src/main/java/com/xg/config/WebSecurity.java
@@ -1,15 +1,16 @@
 package com.xg.config;
 
 import com.xg.config.security.SecurityInterceptorFilter;
-import javax.annotation.Resource;
+import com.xg.config.security.UserDetailService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+
+import javax.annotation.Resource;
 
 /**
  * security 配置
@@ -28,8 +29,17 @@ public class WebSecurity {
     @Resource
     private SecurityInterceptorFilter securityInterceptorFilter;
 
-    @Override
+      @Resource
+      private UserDetailService userDetailService;
+
+      @Override
+      protected UserDetailsService userDetailsService() {
+          return userDetailService;
+      }
+
+      @Override
     protected void configure(HttpSecurity http) throws Exception {
+//      http.authorizeRequests().and().formLogin().
       http.authorizeRequests()
           .anyRequest().authenticated() //任何请求,登录后可以访问
           .and()
---
+++ src/main/java/com/xg/config/security/UserDetailService.java
@@ -0,0 +1,26 @@
+package com.xg.config.security;
+
+import com.xg.api.model.uc.User;
+import com.xg.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    @Resource
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        if (user == null) {
+            throw new UsernameNotFoundException("user not exist");
+        }
+        return new UserDetail(user);
+    }
+}
